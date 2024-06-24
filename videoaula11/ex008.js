bef252fb207ac41944328af073998fed6569822c
Maria Eduarda Albuquerque <albuquerque.zxl@gmail.com>
2024-06-24 01:23:01 -0300

    Condições

---
+++ videoaula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 34
+console.log(`A velocidade do seu carro é de ${vel} km/h`)
+if(vel > 60){ //condição simples
+    console.log('Você ultrapassou o limite de velocidade permitido e por isso foi MULTADO')
+}
+console.log('Dirija sempre usando o cinto de segurança!')
---
+++ videoaula11/ex009.js
@@ -0,0 +1,8 @@
+var pais = 'brasil'
+console.log(`Vivendo em ${pais}`)
+if(pais !="brasil") {
+    console.log('Você é estrangeiro')
+}
+else{
+    console.log('Você é brasileiro')
+}
