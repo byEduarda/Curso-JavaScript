72caa10ae064778ee4c2a553236866b9d9a5873a
Maria Eduarda Albuquerque <albuquerque.zxl@gmail.com>
2024-06-27 17:31:02 -0300

    atividades: verificador de idade

--- videoaula12ex/ex015/script.js
+++ videoaula12ex/ex015/script.js
@@ -15,31 +15,31 @@
                 gênero = 'Homem'
                 if(idade >= 0 && idade < 10) {
                     //criança
-                    img.setAttribute('src', 'foto-bebe-m.png.')
+                    img.setAttribute('src', 'bebe-m.png')
                 } else if(idade <= 21) {
                     //jovem
-                    img.setAttribute('src', 'foto-jovem-m.png')
+                    img.setAttribute('src', 'jovem-m.png')
                 } else if(idade <= 50) {
                     //adulto
-                    img.setAttribute('src', 'foto-adulto-m.png')
+                    img.setAttribute('src', 'adulto-m.png')
                 } else {
                     //idoso
-                    img.setAttribute('src', 'foto-idoso-m.png')
+                    img.setAttribute('src', 'idoso-m.png')
                 }     
             } else if (fsex[1].checked) {
                 gênero = 'Mulher'
                 if(idade >= 0 && idade <10) {
                     //criança
-                    img.setAttribute('src', 'foto-bebe-f.png')
+                    img.setAttribute('src', 'bebe-f.png')
                 } else if(idade <= 21) {
                     //jovem
-                    img.setAttribute('src', 'foto-jovem-f.png')
+                    img.setAttribute('src', 'jovem-f.png')
                 } else if(idade <= 50) {
                     //adulto
-                    img.setAttribute('img', 'foto-adulto-f.png')
+                    img.setAttribute('src', 'adulto-f.png')
                 } else {
                     //idoso
-                    img.setAttribute('img', 'foto-idoso-f.png')
+                    img.setAttribute('src', 'idoso-f.png')
                 }     
             }
             res.style.textAlign = 'center'
