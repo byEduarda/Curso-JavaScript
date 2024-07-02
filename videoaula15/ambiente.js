e88cee93647ee086a70e78fa2fed76ca2df863d3
Maria Eduarda Albuquerque <albuquerque.zxl@gmail.com>
2024-07-02 16:58:47 -0300

    Update ambiente.js

--- videoaula15/ambiente.js
+++ videoaula15/ambiente.js
@@ -4,6 +4,3 @@ num.sort()
 console.log(num)
 console.log(`O vetor tem ${num.length} elementos`)
 console.log(`O primeiro valor do vetor é ${num[0]}`)
-
-let number = [3,4,63,2,142,5,63,23,5,3,]
-number.sort()
