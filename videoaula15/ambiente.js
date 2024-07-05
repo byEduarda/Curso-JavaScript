2eff8d99d48db1c4c65860f862ae5c357c823cba
Maria Eduarda Albuquerque <albuquerque.zxl@gmail.com>
2024-07-05 17:34:47 -0300

    Update ambiente.js

--- videoaula15/ambiente.js
+++ videoaula15/ambiente.js
@@ -1,7 +1,14 @@
 let num = [2,8,9,7]
 num.sort()
-num.push(2)
+num.push(10)
 
 console.log(num)
 console.log(`O vetor tem ${num.length} elementos`)
 console.log(`O primeiro valor do vetor é o número ${num[0]}`)
+
+console.log(num[0])
+console.log(num[1])
+console.log(num[2])
+console.log(num[3])
+console.log(num[4])
+
