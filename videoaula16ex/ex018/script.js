f59573f2ba846fe8fc916d21bda42617339d196d
Maria Eduarda Albuquerque <albuquerque.zxl@gmail.com>
2024-07-07 20:01:52 -0300

    0

---
+++ videoaula16ex/ex018/script.js
@@ -0,0 +1,20 @@
+let num = document.getElementById("txtn")
+let flist = document.getElementById("flist")
+let res = document.getElementById("res")
+let valores = []
+
+function isNumero(n) {
+
+}
+
+function inLista(n, 1){
+
+}
+
+function adicionar() {
+    if(isNumero(num.value) && !inLista(num.value)) {
+
+    } else {
+        window.alert("Valor inválido ou já adicionado na lista")
+    }
+}
