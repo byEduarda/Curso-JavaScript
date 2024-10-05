dd42d691ee23ffee7f3187666ec0813d8fce2a1b
Eduarda Albuquerque <albuquerque.zxl@gmail.com>
2024-10-05 12:12:25 -0300

    Update script.js

--- videoaula16ex/ex018/script.js
+++ videoaula16ex/ex018/script.js
@@ -24,7 +24,7 @@ function adicionar() {
         valores.push(Number(num.value))
         let item = document.createElement("option")
         item.text = `Valor ${num.value} adicionado.`
-        lista.appendChild(item)
+        lista.appendChild(item) 
         res.innerHTML = ''
 
     } else {
